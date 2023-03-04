9c7b49ef48041b2ea2186f8ab2b876fcee8040dc
andreiVasilkevich <andrey.vasilkevich@bk.ru>
2023-03-04 18:57:35 +0300

    homes

--- hws2-master/src/s2-homeworks/hw07/HW7.tsx
+++ hws2-master/src/s2-homeworks/hw07/HW7.tsx
@@ -23,10 +23,6 @@ const HW7 = () => {
 
     return (
         <div id={'hw7'}>
-            <p>HELLO WORLD!!!</p>
-            <p>HELLO WORLD!!!</p>
-            <p>HELLO WORLD!!!</p>
-
             <div className={s2.hwTitle}>Homework #7</div>
 
             {/*демонстрация возможностей компонент:*/}
