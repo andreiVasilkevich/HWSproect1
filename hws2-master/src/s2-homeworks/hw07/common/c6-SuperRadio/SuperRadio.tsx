183d5b46c7ef4df30be2886ca9d75decfb901d86
andreiVasilkevich <andrey.vasilkevich@bk.ru>
2023-03-04 21:29:13 +0300

    homes

--- hws2-master/src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ hws2-master/src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -48,7 +48,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       id={id + '-input-' + o.id}
                       className={finalRadioClassName}
                       type={'radio'}
-                      name={o.name}
+                      name={o.id}
                       checked={o.id === value && true}
                       value={o.id}
                       // name, checked, value делают студенты
