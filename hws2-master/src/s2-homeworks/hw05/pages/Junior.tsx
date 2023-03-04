93b7d819b137cee621353cf7807b6fb937384047
andreiVasilkevich <andrey.vasilkevich@bk.ru>
2023-03-04 19:06:23 +0300

    homes

--- hws2-master/src/s2-homeworks/hw05/pages/Junior.tsx
+++ hws2-master/src/s2-homeworks/hw05/pages/Junior.tsx
@@ -10,7 +10,7 @@ import HW8 from "../../hw08/HW8";
 function Junior() {
     return (
         <div id={'hw5-page-junior'}>
-            {/*<HW6 />*/}
+            <HW6 />
             <HW7 />
             <HW8 />
             {/*<HW9 />*/}
