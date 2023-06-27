a433c4381f0e2635f5a7466b7a7e3d5a9d5032f7
andreiVasilkevich <andrey.vasilkevich@bk.ru>
2023-06-27 15:17:24 +0300

    timerID

--- hws2-master/src/s2-homeworks/hw09/Clock.tsx
+++ hws2-master/src/s2-homeworks/hw09/Clock.tsx
@@ -94,14 +94,14 @@ function Clock() {
             <div className={s.buttonsContainer}>
                 <SuperButton
                     id={'hw9-button-start'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер запущен
+                    disabled={!!timerId} // пишут студенты // задизэйблить если таймер запущен
                     onClick={start}
                 >
                     start
                 </SuperButton>
                 <SuperButton
                     id={'hw9-button-stop'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер не запущен
+                    disabled={!timerId} // пишут студенты // задизэйблить если таймер не запущен
                     onClick={stop}
                 >
                     stop
