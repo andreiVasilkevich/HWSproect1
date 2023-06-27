8415f0651d81d8adda2232df6638ef2ad5008610
andreiVasilkevich <andrey.vasilkevich@bk.ru>
2023-06-27 16:13:41 +0300

    ss

--- hws2-master/src/s2-homeworks/hw09/Clock.tsx
+++ hws2-master/src/s2-homeworks/hw09/Clock.tsx
@@ -28,6 +28,7 @@ function Clock() {
     }
 
     const stop = () => {
+        start()
         // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
         clearTimeout(timerId)
         //setShow(false)
@@ -101,7 +102,7 @@ function Clock() {
                 </SuperButton>
                 <SuperButton
                     id={'hw9-button-stop'}
-                    disabled={false} // пишут студенты // задизэйблить если таймер не запущен
+                    disabled={!timerId} // пишут студенты // задизэйблить если таймер не запущен
                     onClick={stop}
                 >
                     stop
